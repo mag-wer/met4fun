ff2c647cda1ef2d5a34eaf25e34dc5d8bbed58b2
Mag Wer <marc.werner211@gmail.com>
2023-03-20 09:10:38 +0100

    a new calculation

---
+++ met444/datatest.R
@@ -0,0 +1,14 @@
+# A new script 
+
+library(usethis)
+
+
+use_git_config(user.name = "Mag Wer", user.email = "marc.werner211@gmail.com")
+
+
+usethis::git_remotes()
+
+library(gitcreds)
+gitcreds_set()
+
+#so this is a new changed and should be commited?
